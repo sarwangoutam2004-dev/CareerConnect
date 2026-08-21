945f2683dce9b4381245059684c2878611cb2520
sarwangoutam2004-dev <goutam2004@gmail.com>
2026-08-21 20:31:38 +0530

    Configure PostgreSQL database

--- careerconnect/settings.py
+++ careerconnect/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 import os
+import dj_database_url
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
@@ -79,10 +80,10 @@ WSGI_APPLICATION = 'careerconnect.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
